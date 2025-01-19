e9bbcd856d54ed2a55bc551d1390d72015f73a2b
electriccheffer <electriccheff@gmail.com>
2025-01-19 00:41:36 -0600

    CREATED a scene with terrain and trees

--- Assets/Scripts/RandomWalker.cs
+++ Assets/Scripts/RandomWalker.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PerlinWalkerScript : MonoBehaviour
 {
    
-    private float xCoordinate = 0f; 
-    private float zCoordinate = 0.5f; 
-    private float yCoordinate = 0.3f; 
+    private float xCoordinate = 3.0f; 
+    private float zCoordinate = 2.3f; 
+    private float yCoordinate = 0.0f;
+
     private float incrementAmount = 0.001f; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,15 +18,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {	
-
 	float newX = perlinStep(ref xCoordinate); 	
 	float newZ = perlinStep(ref zCoordinate); 
 	float newY = perlinStep(ref yCoordinate);
 	this.transform.position = new Vector3(newX,newY,newZ);
-	if(Input.GetKeyDown(KeyCode.Space))
-	{
-		takeStep();         
-	}
     }
 
     private float perlinStep(ref float coordinate)
@@ -36,25 +32,4 @@ public class NewMonoBehaviourScript : MonoBehaviour
 	return newValue; 
     }
 
-    private void takeStep()
-    {
-	float outcome = Random.Range(0f,1f);
-	if(outcome < 0.1f)
-	{
-		this.transform.position += new Vector3(0,0,1); 
-	}
-	else if(outcome < 0.3f)
-	{
-		this.transform.position += new Vector3(1,0,0);
-	}
-	else if(outcome < 0.8f)
-	{
-		this.transform.position += new Vector3(-1,0,0); 
-	}
-	else
-	{
-		this.transform.position += new Vector3(0,0-1);
-	}
-
-    }
 }
