e75755b996602f16207d35ae983069bdde4ebfdd
electriccheffer <electriccheff@gmail.com>
2025-01-31 11:46:57 -0600

    IMPLEMENTED MOVER for CUBE

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
  
+    public Vector3 velocity = new Vector3(0f,0f,0f); 	
+    public Vector3 accelleration = new Vector3(1f,0f,0f);
 	
-
    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+                
     }
 
     // Update is called once per frame
     void Update()
     {
         
+	if(Input.GetKey(KeyCode.Space)){
+	
+		this.velocity += this.accelleration; 
+                Debug.Log("Velocity: " + this.velocity); 
+		this.transform.position += this.velocity; 
+                this.accelleration = new Vector3(0f,0f,0f);
+		Debug.Log("Accelleration: " + this.accelleration);
+        }
+
     }
 }
