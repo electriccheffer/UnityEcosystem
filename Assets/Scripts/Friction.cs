6e5a2f5dc31bfedc2997844fec627d1f26e27b26
Chris Summa <csumma@uno.edu>
2025-02-07 20:35:30 -0600

    I give up

--- Assets/Scripts/Friction.cs
+++ Assets/Scripts/Friction.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class Friction : MonoBehaviour
 {
+    public float frictionCoefficient;
+     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+	     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    	 
     }
+
+
+   public float getForce(){
+
+	return frictionCoefficient; 
+	
+   }
 }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,11 +4,13 @@ public class Mover : MonoBehaviour
 {
     private Vector3 accelleration = new Vector3(0f,0f,0f);
     private Vector3 velocity = new Vector3(0f,0f,0f);
-    public float  movementAmount = 1.0f;  
+    public float  movementAmount;  
     private Vector3 forward;
     private Vector3 backward;
     private Vector3 left; 
     private Vector3 right;
+    private float friction; 
+    public float frictionMagnitudeThreshold; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,31 +19,39 @@ public class Mover : MonoBehaviour
 	this.backward = new Vector3(-movementAmount,0f,0f); 
 	this.left = new Vector3(0f,0f,movementAmount);   	
 	this.right = new Vector3(0f,0f,-movementAmount);     
+	
     }
 
     // Update is called once per frame
     void Update()
     {
-	
-	if(Input.GetKey(KeyCode.W)){
-		MoveInDirection(forward);
+		
+	if(Input.GetKeyDown(KeyCode.W)){
+		ApplyFriction(this.forward);
 	}
-	else if(Input.GetKey(KeyCode.S)){
-
-		MoveInDirection(backward);
+	else if(Input.GetKeyDown(KeyCode.S)){
+		ApplyFriction(this.backward);
 	}
-	else if(Input.GetKey(KeyCode.D)){
-	
-		MoveInDirection(right);
-
+	else if(Input.GetKeyDown(KeyCode.D)){
+		ApplyFriction(this.right);
 	}
-	else if(Input.GetKey(KeyCode.A)){
-		MoveInDirection(left);
-
+	else if(Input.GetKeyDown(KeyCode.A)){
+		ApplyFriction(this.left);
 	}        
-   	this.accelleration = new Vector3(0f,0f,0f); 
-        this.velocity += this.accelleration;
-    	this.transform.position += this.velocity;
+	else{
+	
+		if(accelleration.magnitude > frictionMagnitudeThreshold){
+		
+			ApplyFriction(this.accelleration);
+		}	
+		else{
+			this.accelleration = Vector3.zero; 
+			this.velocity = this.accelleration; 
+			this.transform.position += this.velocity;
+ 
+		}	
+	} 
+	
     }
 
     void MoveInDirection(Vector3 direction){
@@ -49,5 +59,22 @@ public class Mover : MonoBehaviour
 	this.accelleration += direction; 
 	this.velocity += this.accelleration; 		
 	this.transform.position += this.velocity; 
+    	Debug.Log("Current accelleration: " + this.accelleration);
+	Debug.Log("Current velocity: " + this.velocity);
+	Debug.Log("Current position: " + this.transform.position);
+    }
+    
+    void ApplyFriction(Vector3 currentDirection){
+	Vector3 currentDirectionForce = currentDirection.normalized; 	
+	Debug.Log("Normalized Direction: " +  currentDirectionForce );
+	Debug.Log("Current friction force: " + (-this.friction * currentDirectionForce));
+	MoveInDirection(currentDirection - (this.friction * currentDirectionForce));		
+
+    }
+
+    void OnCollisionEnter(Collision groundCollision){
+
+	this.friction = groundCollision.gameObject.GetComponent<Friction>().getForce(); 
+	Debug.Log("Current Friction " + this.friction);	
     }
 }
